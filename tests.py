a989c908b94b26470a230e73a06dca1cce94dcaa
soderqw <soderqvistw@gmail.com>
2022-01-26 22:17:13 +0100

    Adds test for LIC1, checking both True and False case

--- tests.py
+++ tests.py
@@ -37,6 +37,33 @@ class TestLIC(unittest.TestCase):
         CMV = cmv(parameters, coordinates)
         self.assertTrue(CMV.LIC_0())
 
+    def test_LIC1(self):
+        # False: If all 3-sets of consecutive datapoints
+        # can be contained within the radius
+        # True: If there exists at least one 3-set of 
+        # consecutive datapoints that cannot be contained
+
+        # The set of 3 datapoints should be able to be contained 
+        # within the set radius andShould yield False
+        parameters = PARAMETERS_T()
+        parameters.radius1 = 3
+        coordinates = np.zeros((3, 2))
+        for i in range(len(coordinates)):
+            coordinates[i] = [i,i]
+
+        CMV = cmv(parameters, coordinates)
+        self.assertFalse(CMV.LIC_1())
+        
+        # The set of 3 datapoints should NOT be able to be able
+        # to be contained within the set radius and should yield True
+        parameters.radius1 = 0
+        coordinates = np.zeros((3, 2))
+        for i in range(len(coordinates)):
+            coordinates[i] = [i,i]
+
+        CMV = cmv(parameters, coordinates)
+        self.assertTrue(CMV.LIC_1())
+
 if __name__ == '__main__':
     unittest.main()
         
