4af182dfb582cce6e46c20445f830db11650ce70
Tim Jonsson <timjonss@kth.se>
2022-01-28 10:53:31 +0100

    Added code for LIC7. Computed as LIC0, could probably be refactored

--- cmv.py
+++ cmv.py
@@ -1,12 +1,13 @@
 import numpy as np
-class CMV:
-    def __init__(self, PARAMS, coordinates)
+class cmv:
+    def __init__(self, PARAMS, coordinates):
         self.PARAMS = PARAMS # Check main file for structure
         self.coordinates = coordinates
         self.CondVector = np.array(15, dtype=bool)
 
     # Set Condvector[0]
     def LIC_0():
+        return 0
     
         # Set Condvector[1]
     def LIC_1():
@@ -33,8 +34,18 @@ class CMV:
         return 0
 
     # Set Condvector[7]
-    def LIC_7():
-        return 0
+    def LIC_7(self):
+        if len(self.coordinates) < 3:
+            return False
+        ## This can probably be refactored with LIC0
+        k_Pts = self.PARAMS.k_Pts
+        for i in range(len(self.coordinates)-self.PARAMS.k_Pts - 1):
+            x = self.coordinates[i, 0] - self.coordinates[i+k_Pts+1, 0]
+            y = self.coordinates[i, 1] - self.coordinates[i+k_Pts+1, 1]
+            if np.sqrt(x ** 2 + y ** 2) > self.PARAMS.length1:
+                return True
+        return False
+        
 
     # Set Condvector[8]
     def LIC_8():
