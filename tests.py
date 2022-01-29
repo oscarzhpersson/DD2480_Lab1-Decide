79793a3d178eccae6d0f3ce3c25cb6976664c272
Mustafa Ali <62649206+mustafamusse@users.noreply.github.com>
2022-01-29 12:23:48 +0100

    Please enter the commit message for your changes. Lines starting

--- tests.py
+++ tests.py
@@ -75,6 +75,20 @@ class TestLIC(unittest.TestCase):
         CMV = cmv(parameters, coordinates)
         self.assertTrue(CMV.LIC_1())
 
+    def test_LIC2(self):
+
+       #Test 1: The input will two lines with a 90 degree angle and epsilon set to 0
+       #This should yield false
+        parameters = PARAMETERS_T()
+        parameters.epsilon = np.pi
+        coordinates = np.zeros((3, 2))
+
+        coordinates[0] = [0,0]
+        coordinates[1] = [1,0]
+        coordinates[2] = [2,0]
+
+        CMV = cmv(parameters, coordinates)
+        self.assertTrue(CMV.LIC_2)
 
     # Tests the LIC3 function of the CMV component.
     def test_LIC3(self):
