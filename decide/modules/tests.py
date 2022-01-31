ea748bb96a85f4aa20a27f99a35b0d1c59cb4c40
Mustafa Ali <62649206+mustafamusse@users.noreply.github.com>
2022-01-31 16:25:48 +0100

    Adds the first test case for LIC_2

--- decide/modules/tests.py
+++ decide/modules/tests.py
@@ -76,6 +76,19 @@ class TestLIC(unittest.TestCase):
         CMV = cmv(parameters, coordinates)
         self.assertTrue(CMV.LIC_1())
 
+    def test_LIC2(self):
+        # Test 1: returns false because angle is larger than pi-epsilon, epsilon is set to 1
+        parameters = PARAMETERS_T()
+        parameters.epsilon = np.pi - 1
+        coordinates = np.zeros((3, 2))
+
+        coordinates[0] = [0, 0]
+        coordinates[1] = [1, 0]
+        coordinates[2] = [2, 0]
+
+        CMV = cmv(parameters, coordinates)
+        self.assertFalse(CMV.LIC_2())
+
 
     # Tests the LIC3 function of the CMV component.
     def test_LIC3(self):
@@ -132,8 +145,8 @@ class TestLIC(unittest.TestCase):
 
         coordinates = np.zeros((3, 2))
         coordinates[0] = [1, 1]
-        coordinates[1] = [-1, 0]
-        coordinates[2] = [0, -1]
+        coordinates[1] = [-1, 1]
+        coordinates[2] = [1, -1]
 
         CMV = cmv(parameters, coordinates)
         self.assertTrue(CMV.LIC_4())
