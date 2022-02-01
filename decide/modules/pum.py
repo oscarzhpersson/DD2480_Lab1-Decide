457809997d87e4e4d7e546a5d41525792d3dc34e
Tim Jonsson <timjonss@kth.se>
2022-02-01 14:50:54 +0100

    add - Adds logic to compute PUM

--- decide/modules/pum.py
+++ decide/modules/pum.py
@@ -1,5 +1,24 @@
+import numpy as np
+
 class pum:
-    def __init__(self, LCM, PUV):
-        ## 15x15 array
+    def __init__(self, CMV, LCM, PUV):
+        ## 15x15 array [1 = ANDD, 0 = ORR, -1 = NOTUSED]
+        self.CMV = CMV
         self.LCM = LCM
         self.PUV = PUV
+
+    def compute_PUM(self):
+        PUM = np.zeros(15, 15, dtype=bool)
+        for i in range(self.LCM.shape[0]):
+            for j in range(self.LCM.shape[0]):
+                if i == j:
+                    continue
+                op = self.LCM[i,j]
+                if op == 1:
+                    PUM[i,j] = self.CMV[i] and self.CMV[j]
+                if op == 0:
+                    PUM[i,j] = self.CMV[i] or self.CMV[j]
+                if op == -1:
+                    PUM[i,j] = False
+
+        return PUM
