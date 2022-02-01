975baf812e5099116a807677772a1521f108f640
Mustafa Ali <62649206+mustafamusse@users.noreply.github.com>
2022-02-02 00:53:26 +0100

    docs - adds documentation for the PUM, FUV and Launch functions

--- decide/modules/pum.py
+++ decide/modules/pum.py
@@ -8,7 +8,20 @@ class pum:
         self.PUV = PUV
         self.FUV = FUV
 
+    #Creates the PUM
     def compute_PUM(self):
+        """ The Conditions Met Vector (CMV) can now be used in conjunction with the Logical Connector Matrix (LCM) to form the Preliminary Unlocking Matrix (PUM)
+
+            Parameters
+            -----
+            PUM: Preliminary Unlocking Matrix (PUM)
+
+            Returns
+            -----
+            a PUM Matrix
+
+            """
+
         PUM = np.ones(15, 15, dtype=bool)
         for i in range(self.LCM.shape[0]):
             for j in range(self.LCM.shape[0]):
@@ -24,8 +37,19 @@ class pum:
 
         return PUM
 
-
+    #Creates the FUV
     def compute_FUV(self, PUM):
+        """ The Final Unlocking Vector (FUV) is generated from the Preliminary Unlocking Matrix. The input PUV indicates whether the corresponding LIC is to be considered as a factor in signaling interceptor launch.
+
+            Parameters
+            -----
+            FUV: Final Unlocking Vector (FUV)
+
+            Returns
+            -----
+            The FUV vector
+
+            """
         FUV = np.zeros(15, dtype=bool)
         for i in range(len(FUV)):
             if not self.PUV[i]:
@@ -35,5 +59,18 @@ class pum:
 
         return FUV
 
+    #Checks that all launch conditions are satisfied before launch
     def Launch(self, FUV):
+        """
+        The final launch/no launch decision is based on the FUV. The decision to launch requires that all elements in the FUV be true, i.e. LAUNCH
+
+            Parameters
+            -----
+            FUV: Final Unlocking Vector (FUV)
+
+            Returns
+            -----
+            Returns True or False
+
+            """
         return np.all(FUV)
